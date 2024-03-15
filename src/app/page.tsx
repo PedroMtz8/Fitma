1be60ed0182762aeaf039ef9db8e3e180f1b4080
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-03-14 19:50:36 -0600

    display others cursors setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Room } from './Room';
+import Live from '@/components/Live';
 
 export default async function Home() {
   return (
-    <div className='h-[100vh] w-full flex justify-center items-center '>
-      <h1 className='text-2xl text-white'>Hello World</h1>
+    <div >
+      <Live />
     </div>
   );
 }
